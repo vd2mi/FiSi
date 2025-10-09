cd7e6eb0c5f6e42663490560c8d8a6438cc1c510
abdulrahman alahmed <Abdul.Alahmed2004@gmail.com>
2025-10-09 06:29:43 +0300

    Add Alpha Vantage for free chart data

--- api/stocks.js
+++ api/stocks.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const FINNHUB_API_KEY = process.env.FINNHUB_API_KEY;
+const ALPHAVANTAGE_API_KEY = process.env.ALPHAVANTAGE_API_KEY;
 const BASE_URL = 'https://finnhub.io/api/v1';
 
 module.exports = async (req, res) => {
@@ -31,6 +32,49 @@ module.exports = async (req, res) => {
         break;
 
       case 'candles':
+        if (ALPHAVANTAGE_API_KEY) {
+          const interval = resolution === '5' ? '5min' : resolution === '60' ? '60min' : 'daily';
+          const func = interval.includes('min') ? 'TIME_SERIES_INTRADAY' : 'TIME_SERIES_DAILY';
+          
+          const avResponse = await axios.get('https://www.alphavantage.co/query', {
+            params: {
+              function: func,
+              symbol,
+              interval: interval.includes('min') ? interval : undefined,
+              outputsize: 'full',
+              apikey: ALPHAVANTAGE_API_KEY
+            }
+          });
+
+          const timeSeriesKey = Object.keys(avResponse.data).find(key => key.includes('Time Series'));
+          const timeSeries = avResponse.data[timeSeriesKey];
+          
+          if (timeSeries) {
+            const candles = Object.entries(timeSeries)
+              .map(([time, data]) => ({
+                t: new Date(time).getTime() / 1000,
+                o: parseFloat(data['1. open']),
+                h: parseFloat(data['2. high']),
+                l: parseFloat(data['3. low']),
+                c: parseFloat(data['4. close']),
+                v: parseInt(data['5. volume'])
+              }))
+              .sort((a, b) => a.t - b.t);
+
+            return res.status(200).json({
+              data: {
+                s: 'ok',
+                t: candles.map(c => c.t),
+                o: candles.map(c => c.o),
+                h: candles.map(c => c.h),
+                l: candles.map(c => c.l),
+                c: candles.map(c => c.c),
+                v: candles.map(c => c.v)
+              }
+            });
+          }
+        }
+        
         response = await axios.get(`${BASE_URL}/stock/candle`, {
           params: { symbol, resolution, from, to, token: FINNHUB_API_KEY }
         });
