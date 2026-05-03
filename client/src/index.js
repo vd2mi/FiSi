f69c4f870047ff33ecdcdde391a522f429449f73
abdulrahman alahmed <Abdul.Alahmed2004@gmail.com>
2026-05-03 18:51:31 +0300

    added vervcel analyitics

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 
@@ -7,5 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
